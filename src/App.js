4adb5c921e977006b1afc67a7a3b2c7081df41e7
Oleh Masiuk <olh.masiuk@gmail.com>
2023-05-14 22:46:15 +0300

    with filtering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import DimCustomerLevelsTable from './Data/DimCustomerLevelsTable';
+import FactShowTable from './Data/FactShow';
 
 function App() {
   return (
     <div className="App">
-      <h1>Calibers</h1>
-      <DimCustomerLevelsTable />
+      <h1>FactShow</h1>
+      {/* <DimCustomerLevelsTable /> */}
+      <FactShowTable />
     </div>
   );
 }
